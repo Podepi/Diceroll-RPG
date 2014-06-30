d4c923a2e49ef76518d1372dd1f2f358c1a48719
TarrielDRC <tarriel345@gmail.com>
2014-06-30 17:58:37 +1000

    items in list instead of slots, still WIP buttons now clickable divs reset() removed reset() functionality is now init() and battle() updateitems() function to update item list 3 buttons now added fireball.png, iceball.png, m_missile.png added external.js

---
+++ external.js
@@ -0,0 +1,15 @@
+
+
+function weapontype(i) {
+    switch (i) {
+      case 001:
+        attack = 2;
+        break;
+      case 002:
+        attack = 5;
+        break;
+      }
+}
+function calculateDamage(a) {
+    
+}
